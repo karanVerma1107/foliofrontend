e4f57b8c9aaa4498546b648abb0d2973cf3e8b62
karanVerma1107 <157015568+karanVerma1107@users.noreply.github.com>
2024-10-23 14:28:29 +0530

    pushing

---
+++ src/reducers/followReducers.js
@@ -0,0 +1,64 @@
+import { GET_USER_FOLLOWERS_REQUEST,
+    GET_USER_FOLLOWERS_SUCCESS,
+    GET_USER_FOLLOWERS_FAILURE,
+    GET_USER_FOLLOWING_REQUEST,
+    GET_USER_FOLLOWING_SUCCESS,
+    GET_USER_FOLLOWING_FAILURE,
+ } from "../constants/userprofile";
+
+const initialState = {
+    followers: [],
+    following: [],
+    loadingFollowers: false,
+    loadingFollowing: false,
+    error: null,
+};
+
+export const userFollowReducer = (state = initialState, action) => {
+    switch (action.type) {
+        // Followers
+        case GET_USER_FOLLOWERS_REQUEST:
+            return {
+                ...state,
+                loadingFollowers: true,
+                error: null,
+            };
+        case GET_USER_FOLLOWERS_SUCCESS:
+            return {
+                ...state,
+                loadingFollowers: false,
+                followers: action.payload.followers,
+            };
+        case GET_USER_FOLLOWERS_FAILURE:
+            return {
+                ...state,
+                loadingFollowers: false,
+                error: action.payload.error,
+            };
+
+        // Following
+        case GET_USER_FOLLOWING_REQUEST:
+            return {
+                ...state,
+                loadingFollowing: true,
+                error: null,
+            };
+        case GET_USER_FOLLOWING_SUCCESS:
+            return {
+                ...state,
+                loadingFollowing: false,
+                following: action.payload.followings,
+            };
+        case GET_USER_FOLLOWING_FAILURE:
+            return {
+                ...state,
+                loadingFollowing: false,
+                error: action.payload.error,
+            };
+
+        default:
+            return state;
+    }
+};
+
+
