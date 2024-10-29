5001d9d3e467ba4ac26cad77f0c50c751b067ba9
karanVerma1107 <157015568+karanVerma1107@users.noreply.github.com>
2024-10-29 22:26:10 +0530

    ndbbde

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     host: '0.0.0.0',
     proxy:{
       '/api':{
-        target: 'http://localhost:5000',
+        target: 'https://folio-builder.onrender.com',
         changeOrigin: true,
         rewrite: (path)=>path.replace(/^\/api/,'/api')
       }
