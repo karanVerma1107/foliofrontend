7113bba7eac203192cebbd5a01c0e5b01fef17f2
karanVerma1107 <157015568+karanVerma1107@users.noreply.github.com>
2024-10-30 09:30:53 +0530

    pushi

--- axiosInstance.js
+++ axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
     baseURL: import.meta.env.VITE_API_URL, // Use the environment variable
-    
+    withCredentials: true,
   });
   
   export default axiosInstance;
